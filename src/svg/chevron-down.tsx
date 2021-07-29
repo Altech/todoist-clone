74e1daf3f8adbda96cb0d0151253308d8fe7297e
Altech <takeno.sh@gmail.com>
2021-07-29 09:00:18 +0900

    Add icons

---
+++ src/svg/chevron-down.tsx
@@ -0,0 +1,14 @@
+import React, { CSSProperties } from 'react';
+
+export default (props: { style?: CSSProperties }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="-5 -8 24 24"
+    width="24"
+    height="24"
+    preserveAspectRatio="xMinYMin"
+    style={props.style}
+  >
+    <path d="M7.071 5.314l4.95-4.95a1 1 0 1 1 1.414 1.414L7.778 7.435a1 1 0 0 1-1.414 0L.707 1.778A1 1 0 1 1 2.121.364l4.95 4.95z"></path>
+  </svg>
+);
