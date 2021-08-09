b876cce2464fbe05e20f0ee2b5a820e599340ba2
Altech <takeno.sh@gmail.com>
2021-08-09 15:36:23 +0900

    Refactor: use named export

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import { expect } from 'chai';
-import App from './App';
+
+import { App } from './App';
 
 describe('<App>', () => {
   it('renders learn react link', () => {
